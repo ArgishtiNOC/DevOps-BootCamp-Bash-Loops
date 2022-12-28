cc8656a2030c7c6b02d1fa347f1adf9b4736ff87
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-28 16:37:21 +0400

    Update loops1.sh

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Put your code here
+echo "Enter String"
+read s
+len=${#s}
+for ((i=$len-1; i>=0; i--))
+do
+c=${s:$i:1}
+if [[ $c =~ [a-z] ]]; then
+restr=$restr${c^^}
+echo $c
+echo $restr
+elif [[ $c =~ [A-Z] ]]; then
+restr=$restr${c,,}
+echo $c
+fi
+done
+echo "$restr"
